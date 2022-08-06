da5a35e117631ebaa05eb72269b78617836460dc
Tyler Jost <tjost@login1.ls6.tacc.utexas.edu>
2022-08-06 15:37:49 -0500

    improved image pt 2

--- scripts/.ipynb_checkpoints/evaluateSegmentation-checkpoint.py
+++ scripts/.ipynb_checkpoints/evaluateSegmentation-checkpoint.py
@@ -28,20 +28,22 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
 
 # %%
 dataDir = '../data/allCells'
 
-jsonAnnotations = os.path.join(dataDir, 'annotations', 'train.json')
-imgDir = os.path.join(dataDir, 'images')
+if 'cells_train' not in DatasetCatalog:
+    jsonAnnotations = os.path.join(dataDir, 'annotations', 'train.json')
+    imgDir = os.path.join(dataDir, 'images')
 
-jsonAnnotations = os.path.join(dataDir, 'annotations', 'train.json')
-imgDir = os.path.join(dataDir, 'images')
+    register_coco_instances("cells_train", {}, jsonAnnotations, imgDir)
 
-# %%
-from detectron2.data.datasets import register_coco_instances
-register_coco_instances("cells_train", {}, jsonAnnotations, imgDir)
-register_coco_instances("cells_test", {}, jsonAnnotations, imgDir)
+if 'cells_test' not in DatasetCatalog: 
+    jsonAnnotations = os.path.join(dataDir, 'annotations', 'test.json')
+    imgDir = os.path.join(dataDir, 'images')
+
+    register_coco_instances("cells_test", {}, jsonAnnotations, imgDir)
 
 # %%
 cfg = get_cfg()
@@ -71,7 +73,7 @@ predictor = DefaultPredictor(cfg)
 # %%
 from detectron2.utils.visualizer import ColorMode
 
-dataset_dicts = DatasetCatalog['cells_train']()
+dataset_dicts = DatasetCatalog['cells_test']()
 # %%
 # expDir = '../data/AG2021Split16'
 # stage = 'val'
@@ -109,23 +111,3 @@ c = 1
 for d in random.sample(dataset_dicts, 1):
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-
-# %%
-cfg = get_cfg()
-if not torch.cuda.is_available():
-    print('CUDA not available, resorting to CPU')
-    cfg.MODEL.DEVICE='cpu'
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("mcf7_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-cfg.OUTPUT_DIR = '../output/splitMCF10a'
-
-
--- scripts/evaluateSegmentation.py
+++ scripts/evaluateSegmentation.py
@@ -28,20 +28,22 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
 
 # %%
 dataDir = '../data/allCells'
 
-jsonAnnotations = os.path.join(dataDir, 'annotations', 'train.json')
-imgDir = os.path.join(dataDir, 'images')
+if 'cells_train' not in DatasetCatalog:
+    jsonAnnotations = os.path.join(dataDir, 'annotations', 'train.json')
+    imgDir = os.path.join(dataDir, 'images')
 
-jsonAnnotations = os.path.join(dataDir, 'annotations', 'train.json')
-imgDir = os.path.join(dataDir, 'images')
+    register_coco_instances("cells_train", {}, jsonAnnotations, imgDir)
 
-# %%
-from detectron2.data.datasets import register_coco_instances
-register_coco_instances("cells_train", {}, jsonAnnotations, imgDir)
-register_coco_instances("cells_test", {}, jsonAnnotations, imgDir)
+if 'cells_test' not in DatasetCatalog: 
+    jsonAnnotations = os.path.join(dataDir, 'annotations', 'test.json')
+    imgDir = os.path.join(dataDir, 'images')
+
+    register_coco_instances("cells_test", {}, jsonAnnotations, imgDir)
 
 # %%
 cfg = get_cfg()
@@ -71,15 +73,28 @@ predictor = DefaultPredictor(cfg)
 # %%
 from detectron2.utils.visualizer import ColorMode
 
-dataset_dicts = DatasetCatalog['cells_train']()
+dataset_dicts = DatasetCatalog['cells_test']()
 # %%
 # expDir = '../data/AG2021Split16'
 # stage = 'val'
 
 # inputs = [expDir, stage]
+
+params = {"ytick.color" : "w",
+          "xtick.color" : "w",
+          "axes.labelcolor" : "w",
+          "axes.edgecolor" : "w",
+          "text.color": "w"}
+plt.rcParams.update(params)
+
+plt.style.use('dark_background')
+
 random.seed(123)
 c = 1
-for d in random.sample(dataset_dicts, 5):
+nIms = 5
+imSpace = 1
+fig = plt.figure(figsize=(10,10))
+for d in random.sample(dataset_dicts, nIms): 
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
     v = Visualizer(im[:, :, ::-1],
@@ -88,15 +103,18 @@ for d in random.sample(dataset_dicts, 5):
                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
     )
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    plt.figure(figsize=(10,10))
-    plt.subplot(121)
+    plt.subplot(nIms, 2, imSpace)
     plt.imshow(im)
+    imSpace+=1
     plt.title('Testing Image Input')
-    plt.subplot(122)
+    plt.subplot(nIms, 2, imSpace)
     plt.imshow(out.get_image()[:, :, ::-1])
     plt.title('Model Output')
-    plt.show()
-    c+=1
+    imSpace+=1
+plt.savefig('../media/sampleSegmentation.png',bbox_inches='tight')
+
+# plt.show()   
+
 
 # %%
 # %%timeit
@@ -109,23 +127,3 @@ c = 1
 for d in random.sample(dataset_dicts, 1):
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-
-# %%
-cfg = get_cfg()
-if not torch.cuda.is_available():
-    print('CUDA not available, resorting to CPU')
-    cfg.MODEL.DEVICE='cpu'
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("mcf7_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-cfg.OUTPUT_DIR = '../output/splitMCF10a'
-
-
